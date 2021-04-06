bf0528acc8ce3cbab9f166dde7317842f6c5ecea
Burak Akcal <200431686@student.georgianc.on.ca>
2021-04-05 21:22:03 -0400

    handlebars installation and trying to implement

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -21,7 +21,7 @@ const Register = ({ setAlert, register, isAuthenticated }) => {
   const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
-      setAlert('Passwords do not match', 'danger');
+      setAlert('The given passwords do not match!', 'danger');
     } else {
       register({ name, email, password });
     }
